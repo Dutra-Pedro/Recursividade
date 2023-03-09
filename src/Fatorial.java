bf46fb0e849a52a6be7dd1a29bad1635191f92d4
logonrmlocal <dutra.pedrosousa@gmail.com>
2023-03-09 11:09:04 -0300

    oLAÁ

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Fatorial {
-    
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int resp = S.nextInt();
+        int fatorial=1;
+        for (int i=1; i<=resp; i++){
+                fatorial*=i;
+        }
+        System.out.println(fatorial);
+    }
 }
--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,3 +1,7 @@
 public class Fibonacci {
-
+    public static void main(String[] args) {
+        for (int i = 1; i<100; i++){
+            System.out.println((i-1)+i);
+        }
+    }
 }
